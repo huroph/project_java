8596889351d8c76449aadeefa92f3832f300fcec
huroph26 <hugo.nahmias@icloud.com>
2019-05-24 15:05:23 +0200

    Create player class

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -132,7 +132,7 @@ class ViewFrame extends JFrame implements KeyListener {
 		this.addKeyListener(this);
 		this.setContentPane(new ViewPanel(this));
 		//this.setSize(800 + this.getInsets().left + this.getInsets().right, 600 + this.getInsets().top + this.getInsets().bottom);
-		this.setSize(1920,1080);
+		this.setSize(1920,100);
 		this.setLocationRelativeTo(null);
 	}
 
