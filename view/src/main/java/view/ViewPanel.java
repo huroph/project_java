b155fed3740a4596d35d5635e41587660a5d808b
Roméo <romeo.bernat@gmail.com>
2019-05-27 10:12:41 +0200

    test center map finish

--- view/src/main/java/view/ViewPanel.java
+++ view/src/main/java/view/ViewPanel.java
@@ -58,7 +58,7 @@ class ViewPanel extends JPanel implements Observer {
 		Map map = viewFrame.getModel().getMap();
 		Block[][] blocks = map.getBlocks();
 		for(int y = (this.getHeight()/2)-(map.getHeight()/2); y<map.getHeight(); y++){
-			for(int x = 0;x<map.getLenght();x++){
+			for(int x = (this.getWidth()/2)-(map.getLenght()/2);x<map.getLenght();x++){
 				switch (blocks[y][x].getType()){
 					case WALL:
 						graphics.drawImage(viewFrame.getModel().getMap().getSprites(0),blocks[y][x].getPosX(),blocks[y][x].getPosY(), this);
