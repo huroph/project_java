2369f29bee8c954e1d47b1314b6c1f93ae658a5c
Roméo <romeo.bernat@gmail.com>
2019-06-03 10:00:16 +0200

    thread un peu cassé mais mieux

--- model/src/main/java/model/Fall.java
+++ model/src/main/java/model/Fall.java
@@ -42,6 +42,13 @@ public class Fall implements Runnable {
 
                 }
             }
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
