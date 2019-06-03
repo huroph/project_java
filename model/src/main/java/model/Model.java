a0657a204aedcfff5ae92af8781ad9b3c25d4a64
Roméo <romeo.bernat@gmail.com>
2019-06-03 12:29:48 +0200

    Thread qui marche !

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -226,9 +226,21 @@ public final class Model extends Observable implements IModel {
 	public void scanFall(){
 				for(int y = 0; y<getMap().getHeight(); y++){
 					for(int x = 0;x<getMap().getLenght();x++){
+
+						//conditions pour que les glissades des blocks
+						if( (getMap().getBlockTypeAt(x,y).isFall()) && getMap().getBlockTypeAt(x+1,y).equals(BlockType.EMPTY) && getMap().getBlockTypeAt(x+1,y+1).equals(BlockType.EMPTY) && (getMap().getBlockTypeAt(x,y+1).isFall()) ){
+							getMap().setBlockTypeAt(x+1, y, getMap().getBlockTypeAt(x,y));
+							getMap().setBlockTypeAt(x, y, BlockType.EMPTY);
+						}
+						if( (getMap().getBlockTypeAt(x,y).isFall()) && getMap().getBlockTypeAt(x-1,y).equals(BlockType.EMPTY) && getMap().getBlockTypeAt(x-1,y+1).equals(BlockType.EMPTY) && (getMap().getBlockTypeAt(x,y+1).isFall()) ){
+							getMap().setBlockTypeAt(x-1, y, getMap().getBlockTypeAt(x,y));
+							getMap().setBlockTypeAt(x, y, BlockType.EMPTY);
+						}
+
+
 						if( (getMap().getBlockTypeAt(x,y).isFall()) && getMap().getBlockTypeAt(x,y+1).equals(BlockType.EMPTY) ){
 
-						    //si block en dessus d'un rocher ou diament est vide, alors rocher ou diamant tombe
+							//si block en dessous d'un rocher ou diament est vide, alors rocher/diamant tombe
 						    if(this.IndexPos(this.getPlayer().getPosX()) != x || this.IndexPos(this.getPlayer().getPosY()) != y+1) {
 
                                 getMap().setBlockTypeAt(x, y+1, getMap().getBlockTypeAt(x,y));
@@ -237,23 +249,15 @@ public final class Model extends Observable implements IModel {
                             }
 
 						    //si un joueur est deux block en dessous du rocher alors il meurt mais je crois que c'est pas ouf
-                            if(this.IndexPos(this.getPlayer().getPosX()) != x || this.IndexPos(this.getPlayer().getPosY()) != y+2) {
+                            if(this.IndexPos(this.getPlayer().getPosX()) == x && this.IndexPos(this.getPlayer().getPosY()) != y+1 && getMap().getBlockTypeAt(x,y-1).equals(BlockType.EMPTY) ) {
 
-                                //En gros il meurt mais je le ferai plus tard
+                                //MORT!!!
 
                             }
 
 						}
 
-						//conditions pour que les glissades des blocks
-                        if( (getMap().getBlockTypeAt(x,y).isFall()) && getMap().getBlockTypeAt(x+1,y).equals(BlockType.EMPTY) && getMap().getBlockTypeAt(x+1,y+1).equals(BlockType.EMPTY) && (getMap().getBlockTypeAt(x,y+1).isFall()) ){
-                            getMap().setBlockTypeAt(x+1, y, getMap().getBlockTypeAt(x,y));
-                            getMap().setBlockTypeAt(x, y, BlockType.EMPTY);
-                        }
-                        if( (getMap().getBlockTypeAt(x,y).isFall()) && getMap().getBlockTypeAt(x-1,y).equals(BlockType.EMPTY) && getMap().getBlockTypeAt(x-1,y+1).equals(BlockType.EMPTY) && (getMap().getBlockTypeAt(x,y+1).isFall()) ){
-                            getMap().setBlockTypeAt(x-1, y, getMap().getBlockTypeAt(x,y));
-                            getMap().setBlockTypeAt(x, y, BlockType.EMPTY);
-                        }
+
 
 					}
 				}
